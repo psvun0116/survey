5add57a575643fe34e7bf688725718beff9a04fe
Paula Vun <paulavun@Paulas-MacBook-Pro.local>
2019-02-11 21:35:31 -0800

    created survey.html page form to let user fill out name, favorite food, favorite music, date of birth, ethnicity as a select box, marital status as as radio buttons.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var foodInput= $("input#food").val();
+    var musicInput = $("input#music").val();
+    var bornInput = $("input#born").val();
+
+    $(".name").text(nameInput);
+    $(".food").text(addressInput);
+    $(".music").text(locationInput);
+    $(".born").text(bornInput);
+
+    var ethnicity = $("#ethnicity").val();
+
+    var status = $("input:radio[name=status]:checked").val();
+
+  });
+});
